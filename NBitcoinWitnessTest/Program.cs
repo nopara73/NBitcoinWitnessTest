0866e017c303c5f219a7c89928805630472adbf1
nopara73 <adam.ficsor73@gmail.com>
2018-10-17 06:04:03 +0200

    Add project files.

---
+++ NBitcoinWitnessTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NBitcoinWitnessTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
